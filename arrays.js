70506f0d67474bf59e5211353dbf075150ea17bb
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-31 08:11:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+function addElementToBeginningOfArray(chocolateBars, 'foo') {
+  const chocolateBars []
+  return ['foo', ...chocolateBars]
+}
