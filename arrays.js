d99de4bf0f183f83748acd52a22752cb33e11603
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-31 07:32:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(chocolateBars, foo){
   [foo , ...chocolateBars]
-  return chocolateBars 
+  return chocolateBars
 }
 function destructivelyAddElementToBeginningOfArray(chocolateBars, foo){
   chocolateBars.unshift(foo)
