e2a89727cdf7f97f429657848ef00d384d53b363
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-31 08:34:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,6 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
   return chocolateBars
 }
 function removeElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(1)
+  chocolateBars = chocolateBars.slice()
   return chocolateBars
 }
