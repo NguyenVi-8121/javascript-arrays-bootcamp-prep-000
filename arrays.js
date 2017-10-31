e46940ce7dbcc3046326f4a4713700dd9b2fe994
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-31 08:01:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   return chocolateBars
 }
 function removeElementFromEndOfArray(chocolateBars){
-  chocolateBars.slice(chocolateBars.length - 1);
+  chocolateBars.slice(chocolateBars.length - 1)
   return chocolateBars
 }
