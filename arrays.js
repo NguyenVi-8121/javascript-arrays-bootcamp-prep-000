b73701e47df0ec7ebc74dbee4396556da9ea9e13
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-31 07:09:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,6 +16,7 @@ function destructivelyAddElementToEndOfArray(chocolateBars, foo){
   chocolateBars.push(foo)
   return chocolateBars
 }
-function accessElementInArray(chocolateBars){
+function accessElementInArray(chocolateBars, 2){
   console.log(accessElementInArray[2]);
 }
+
